83e2c07d2279a5ec3bb863eb0708706710e9fcd7
Awais <awais.aamir@outlook.com>
2026-09-03 19:05:39 +0100

    Added basics of SQLAlchemy - Engine, Session

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,9 +1,22 @@
 from sqlalchemy import create_engine
 from app.config.settings import get_settings
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
 
 # General Terms:
     # Transaction -> Group of queries, that either compelete entirely or fail completely
+    # Typical DB Connection:
+        # App uses database driver to open a connectiion
+        # network socket is opened to connect to app and db
+        # user is authenticated, opertion completes and connection may be closed
+        # network socket is closed
+    # Pooling DB connections:
+        # Why -> As app scales up, constant opening and closing of connection
+            # becomes more expensive and impact app performance
+        # Sol -> Find a way to keep connections open and pass them when needed
+        # What -> Reduce cost of opening and closing connections by maintaining
+            # pool of connections 
+        # Do not always help -> Expensive to open/close/maintain these connections
 
 # ---- Engine ---- #
 # Create Engine -> Engine is starting point for any SQLAlchemy Application
@@ -14,9 +27,13 @@ from sqlalchemy.orm import Session
         # Connection Pool
 
 # This URL/String passes important info (what db it is, how to locate db..) to engine
+    # returns setting objects, we need to access DB url
 db_url = get_settings()
-engine = create_engine(db_url)
+engine = create_engine(db_url.DATABASE_URL)
 
+# All our entites (item, snapshot etc) share the same SQLAlchemy metadata
+class Base(DeclarativeBase):
+    pass
 
 # ---- Connection ---- #
 # Connection -> Managed by Session in ORM but still nice to know:
@@ -39,8 +56,21 @@ engine = create_engine(db_url)
 # ...     conn.commit()
 
 # ---- ORM & Session -----
-# Fundamental transactional/database interactive object when using ORM is called the session
-# Session makes use of Engine to interact with the db
+# Session -> Estabilishes all conversations with the db and represents holding zone
+ # for all objects which are loaded/associated with it during its span
+ # Provides interface where queries can be made that will return/moidfy ORM mapped obj
+# ORM objects are maintained inside Session inside identity map
+    # Data structure that maintains unique copies of each object by primary id 
+# Session -> once queries issued, requests connection from an Engine
+    # and then estabilishes a transaction on that connection
+    # transaction remains in effect until Session is instructed to commit/rollback
+    # when transaction ends, connection resource is released to connection pool
+# Uses unit of work pattern:
+    # When DB is about to be queried/transaction committed (making db change perm)
+    # Session first flushes all pending changes stored in memory to the database
 
-with Session(engine) as session:
-    
+
+# Create a Session factory -> Can call later whenever a request needs one 
+SessionLocal = sessionmaker(bind=engine)
+
+
